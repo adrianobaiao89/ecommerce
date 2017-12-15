011b8b0bc38fd6789ae0fa10a9f3cb1377f47de5
Adriano Baiao <adrianobaiao89@hotmail.com>
2017-12-15 13:32:22 -0200

    calculando frente

--- site-cart.php
+++ site-cart.php
@@ -9,7 +9,8 @@ $app->get('/cart', function() {
 	$page = new Page();
 	$page->setTpl("cart",[
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>  Cart::clearMsgError()
 	]);
 
 });
@@ -60,6 +61,15 @@ $app->get('/cart/:idproduct/remove', function($idproduct) {
 		header("Location: /cart");
 		exit;
 	});
+
+	$app->post('/cart/freight', function() {
+				
+			$cart = Cart::getFromSession();		
+			$cart->setFreight($_POST['zipcode']);
+		
+			header("Location: /cart");
+			exit;
+		});
 
 
 ?>
